f27f7ad1012347fb3cb695460d04769c5b345b74
Andreas Heiduk <asheiduk@gmail.com>
2018-08-29 21:58:55 +0200

    eclipse oxygen repository urls
    Signed-off-by: Andreas Heiduk <asheiduk@gmail.com>

--- com.ifedorenko.p2browser/src/com/ifedorenko/p2browser/dialogs/RepositoryLocationDialog.java
+++ com.ifedorenko.p2browser/src/com/ifedorenko/p2browser/dialogs/RepositoryLocationDialog.java
@@ -94,6 +94,7 @@ public class RepositoryLocationDialog
     {
         super.create();
 
+        combo.add( "http://download.eclipse.org/releases/oxygen" );
         combo.add( "http://download.eclipse.org/releases/neon" );
         combo.add( "http://download.eclipse.org/releases/mars" );
         combo.add( "http://download.eclipse.org/releases/luna" );
@@ -103,6 +104,7 @@ public class RepositoryLocationDialog
         // combo.add( "http://download.eclipse.org/releases/helios" );
         // combo.add( "http://download.eclipse.org/releases/galileo" );
 
+        combo.add( "http://download.eclipse.org/eclipse/updates/4.7" );
         combo.add( "http://download.eclipse.org/eclipse/updates/4.6" );
         combo.add( "http://download.eclipse.org/eclipse/updates/4.5" );
         combo.add( "http://download.eclipse.org/eclipse/updates/4.4" );
